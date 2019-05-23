57669384abb2fb2999a7f56b82dbd316e45c888d
Wagner Monteiro A Santos <wagner.santos@luizalabs.com>
2019-05-23 15:40:01 -0300

    security

---
+++ src/main/java/br/com/ifitness/commons/beans/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package br.com.ifitness.commons.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .passwordEncoder(new BCryptPasswordEncoder())
+            .withUser("ifitness-web-user")
+            .password("iFWebUser")
+            .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers(
+                "/v1/user/signIn",
+                "/v1/user/signUp",
+                "/v2/api-docs",
+                "/configuration/**",
+                "/swagger*/**",
+                "/webjars/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated().and().csrf().disable();
+    }
+}
