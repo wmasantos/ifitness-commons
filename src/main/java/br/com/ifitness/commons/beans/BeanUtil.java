3c77e303f380c6a2cb87faaedde5d4be2f3f3dcc
Wagner Monteiro A Santos <wagner.santos@luizalabs.com>
2019-05-31 16:45:14 -0300

    security

--- src/main/java/br/com/ifitness/commons/beans/BeanUtil.java
+++ src/main/java/br/com/ifitness/commons/beans/BeanUtil.java
@@ -1,14 +1,47 @@
 package br.com.ifitness.commons.beans;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanUtil {
+
+    @Value("${email.host}")
+    private String emailHost;
+    @Value("${email.port}")
+    private int emailPort;
+    @Value("${email.userName}")
+    private String emailUserName;
+    @Value("${email.password}")
+    private String emailPassword;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(emailHost);
+        mailSender.setPort(emailPort);
+
+        mailSender.setUsername(emailUserName);
+        mailSender.setPassword(emailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
 }
